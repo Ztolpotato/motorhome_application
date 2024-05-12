26ca2966aa9115010c7e800e41557e629a5df28f
Ztolpotato <robin.sorensen@bygdsiljum.se>
2024-05-12 16:34:33 +0200

    thermocouple probably bad, camera not laggy

--- main/model/modules/engineTemp.py
+++ main/model/modules/engineTemp.py
@@ -8,7 +8,7 @@ class engineTemp:
 
     def __init__(self):
         cs = digitalio.DigitalInOut(board.D7)
-        cs.direction = digitalio.Direction.OUTPUT
+        #cs.direction = digitalio.Direction.OUTPUT
         spi = board.SPI()
         global max31855
         max31855 = adafruit_max31855.MAX31855(spi, cs)
@@ -18,7 +18,7 @@ class engineTemp:
          tempC = max31855.temperature 
         except:
             print("Thermocouple sensor error")
-            tempC = 0
+            return -99
         tempC = int(((tempC-17)*100)/91)
         print("Temperature: {} C ".format(tempC))
         return tempC
